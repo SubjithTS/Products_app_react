87cd9e0079ba2d4d058bbefca283a03d7d16f69b
SubjithTS <subjith107@gmail.com>
2024-12-30 13:03:42 +0530

    Prettier

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,68 +1,65 @@
-import React from 'react';
+)import React from "react";
 
 const Form = () => {
-    return (
-	<div>
-	<div className="container">
-    <div className="row">
-      <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-        <div className="row g-3">
-          
-          <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+	return (
+		<div>
+			<div className="container">
+				<div className="row">
+					<div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+						<div className="row g-3">
+							<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+								<label for="" className="form-label">
+									Product Name
+								</label>
+								<input type="text" className="form-control" />
+							</div>
 
-             <label for="" className="form-label">Product Name</label>
-             <input type="text" className="form-control"/>
+							<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+								<label for="" className="form-label">
+									Category
+								</label>
+								<input type="text" className="form-control" />
+							</div>
 
-            </div>
-          
-          <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+							<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+								<label for="" className="form-label">
+									Price
+								</label>
+								<input type="number" className="form-control" />
+							</div>
 
-             <label for="" className="form-label">Category</label>
-             <input type="text" className="form-control"/>
+							<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+								<label for="" className="form-label">
+									Manufacturing Date
+								</label>
+								<input type="date" className="form-control" />
+							</div>
 
-            </div>
-          
-          <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+							<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+								<label for="" className="form-label">
+									Expiry Date
+								</label>
+								<input type="date" className="form-control" />
+							</div>
 
-             <label for="" className="form-label">Price</label>
-             <input type="number" className="form-control"/>
+							<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+								<label for="" className="form-label">
+									Manufacturer
+								</label>
+								<input type="text" className="form-control" />
+							</div>
 
-            </div>
-          
-          <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+							<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+								<button className="btn btn-success">
+									Submit
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-             <label for="" className="form-label">Manufacturing Date</label>
-             <input type="date" className="form-control"/>
-
-            </div>
-          
-          <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-             <label for="" className="form-label">Expiry Date</label>
-             <input type="date" className="form-control"/>
-
-            </div>
-          
-          <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-             <label for="" className="form-label">Manufacturer</label>
-             <input type="text" className="form-control"/>
-
-            </div>
-          
-          <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-            <button className="btn btn-success">Submit</button>
-            
-            </div>
-          
-        </div>
-      </div>
-    </div>
-  </div>
-
-    </div>
-    )
-}
-
-export default Form
+export default Form;
